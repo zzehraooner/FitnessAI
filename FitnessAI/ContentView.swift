d6a86da7cef96398d1b3958763071eeeb47f899c
zehra öner <95606849+zzehraooner@users.noreply.github.com>
2026-07-25 22:45:26 +0300

    Initial Commit

---
+++ FitnessAI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FitnessAI
+//
+//  Created by Zehra Öner on 25.07.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FitnessAI/FitnessAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAIApp.swift
+//  FitnessAI
+//
+//  Created by Zehra Öner on 25.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
